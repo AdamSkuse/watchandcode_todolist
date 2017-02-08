494edc60385f5a321ac8072c80e19d6485b317f6
AdamSkuse <a.t.skuse@gmail.com>
2017-02-08 14:41:41 +0000

    Adds methods on todoList object. Completes v2.

--- todolist.js
+++ todolist.js
@@ -3,18 +3,27 @@ var todoList = {
 	displayTodos: function() {
 		console.log("My todos: ", this.todos);
 	},
-	addTodo: function(todo) {
-		this.todos.push(todo);
+	addTodo: function(todoText) {
+		this.todos.push({
+			todoText: todoText,
+			completed: false
+		});
 		this.displayTodos();
 	},
-	changeTodo: function(index, todo) {
-		this.todos[index] = todo;
+	changeTodo: function(index, todoText) {
+		this.todos[index].todoText = todoText;
 		this.displayTodos();
 	},
 	deleteTodo: function(index) {
 		this.todos.splice(index, 1);
 		this.displayTodos();
+	},
+	toggleCompleted: function(index) {
+		var todo = this.todos[index];
+		todo.completed = !todo.completed;
+		this.displayTodos();
 	}
+
 };
 
 
