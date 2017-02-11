9440a0f1fdda301c9e4e43a602511db6caea560e
AdamSkuse <a.t.skuse@gmail.com>
2017-02-11 17:17:10 +0000

    Completes '- Each <li> should have an id that has the todo position '

--- todolist.js
+++ todolist.js
@@ -82,7 +82,7 @@ var view = {
               } else {
               todoTextWithCompletion = todo.todoText + " () ";
          }
-
+        todoLi.id = i;
         todoLi.textContent = todoTextWithCompletion;
         todoLi.appendChild(this.createDeleteButton());
         todosUl.appendChild(todoLi);
